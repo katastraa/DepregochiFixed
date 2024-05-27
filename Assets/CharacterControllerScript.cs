8faa710c024ac2ad61d8369004274ec1b0c1b064
Luh gonzalez <madeleine2144@gmail.com>
2024-05-27 15:57:58 +0200

    fixed status animations

--- Assets/CharacterControllerScript.cs
+++ Assets/CharacterControllerScript.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class CharacterControllerScript : MonoBehaviour
 {
+    private Animator animator;
+    public TimerScript timerScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timerScript.Overstimulation == true)
+        {
+            animator.SetBool("isStressed", true);
+        }
+        else
+        {
+            animator.SetBool("isStressed", false);
+        }
+
+        if (timerScript.Overthink== true)
+        {
+            animator.SetBool("isOverthinking", true);
+        }
+        else
+        {
+            animator.SetBool("isOverthinking", false);
+        }
     }
 }
